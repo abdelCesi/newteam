1c626afe8e4e1973c25173da243e75d39f7067e9
djole31 <florian.bosc@hotmail.fr>
2015-06-23 12:04:55 +0200

    New commit by Florian

---
+++ app/Model/Container.php
@@ -0,0 +1,194 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Container Model
+ *
+ * @property Place $Place
+ * @property Packing $Packing
+ * @property ArticlesUnit $ArticlesUnit
+ * @property Box $Box
+ * @property Moveorder $Moveorder
+ * @property Pallet $Pallet
+ * @property Receptionorder $Receptionorder
+ * @property Shippingorder $Shippingorder
+ */
+class Container extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'id' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'code' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'status' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'place_id' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'packing_id' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Place' => array(
+			'className' => 'Place',
+			'foreignKey' => 'place_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Packing' => array(
+			'className' => 'Packing',
+			'foreignKey' => 'packing_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'ArticlesUnit' => array(
+			'className' => 'ArticlesUnit',
+			'foreignKey' => 'container_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),
+		'Box' => array(
+			'className' => 'Box',
+			'foreignKey' => 'container_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),
+		'Moveorder' => array(
+			'className' => 'Moveorder',
+			'foreignKey' => 'container_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),
+		'Pallet' => array(
+			'className' => 'Pallet',
+			'foreignKey' => 'container_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+
+/**
+ * hasAndBelongsToMany associations
+ *
+ * @var array
+ */
+	public $hasAndBelongsToMany = array(
+		'Receptionorder' => array(
+			'className' => 'Receptionorder',
+			'joinTable' => 'receptionorders_containers',
+			'foreignKey' => 'container_id',
+			'associationForeignKey' => 'receptionorder_id',
+			'unique' => 'keepExisting',
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+		),
+		'Shippingorder' => array(
+			'className' => 'Shippingorder',
+			'joinTable' => 'shippingorders_containers',
+			'foreignKey' => 'container_id',
+			'associationForeignKey' => 'shippingorder_id',
+			'unique' => 'keepExisting',
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+		)
+	);
+
+}
