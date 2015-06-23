7f65738e139e67072177c9d9813bf8b6afc9eeff
djole31 <florian.bosc@hotmail.fr>
2015-06-23 12:03:07 +0200

    New commit by Florian

---
+++ app/Model/Packing.php
@@ -0,0 +1,113 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Packing Model
+ *
+ * @property LengthTbl $LengthTbl
+ * @property Width $Width
+ * @property Height $Height
+ * @property Container $Container
+ */
+class Packing extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'id' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'length_tbl_id' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'width_id' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'height_id' => array(
+			'notEmpty' => array(
+				'rule' => array('notEmpty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'LengthTbl' => array(
+			'className' => 'LengthTbl',
+			'foreignKey' => 'length_tbl_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Width' => array(
+			'className' => 'Width',
+			'foreignKey' => 'width_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Height' => array(
+			'className' => 'Height',
+			'foreignKey' => 'height_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Container' => array(
+			'className' => 'Container',
+			'foreignKey' => 'packing_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
